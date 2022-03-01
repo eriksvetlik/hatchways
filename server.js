cdb3e8698d9f83c5ba998d6696f6a4d8ec3982d3
eriksvetlik <svetlik.erik@gmail.com>
2022-03-01 09:15:05 -0600

    removed unneeded code

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-// const path = require("path");
 const api = require("./routes/index.js");
 
 const PORT = process.env.PORT || 3000;
@@ -10,14 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/api", api);
-
-// app.use(express.static('public'));
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'index.html'));
-// });
-
-// app.get('/api/pets', (req, res) => res.json(petData));
 
 app.listen(PORT, () => {
   console.log(`listening at http://localhost:${PORT}`);
