cbb44979158be4ef25aefb3e35e8f52adaccc503
eriksvetlik <svetlik.erik@gmail.com>
2022-03-01 11:53:14 -0600

    seperated express API code into app.js

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
-const express = require("express");
-const api = require("./routes/index.js");
+const app = require("./app");
 
 const PORT = 3000;
-
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use("/api", api);
 
 app.listen(PORT, () => {
   console.log(`listening at http://localhost:${PORT}`);
